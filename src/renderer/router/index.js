b9b1ff7308af20ab2632bd810532b4290f4e791f
e <liuxu@yunfucloud.com>
2019-06-15 18:12:32 +0800

    velg add element-ui

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -6,9 +6,16 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
+      path: '/loginModel',
+      component: () => import('@/componets/loginModel/loginModel'), 
+      hidden: false
+    },
+    {
       path: '/',
       name: 'landing-page',
-      component: require('@/components/LandingPage').default
+      component: require('@/components/LandingPage').default,
+      hidden: true
+
     },
     {
       path: '*',
